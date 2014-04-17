b130129466b87b96dfdde7fd54af5b9eb15b0de9
Riley Moses <victi.resurgo@gmail.com>
2014-04-17 14:11:19 -0600

    Add NuGet plugin, add SQLite-Net

---
+++ Core/Model/JSONResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+
+namespace Core
+{
+	public class JSONResponse
+	{
+		// These are attributes
+		[PrimaryKey, AutoIncrement]
+		// This is a property
+		public int Id { get; set; }
+		public String ObjOrArr { get; set; }
+		public bool Empty { get; set; }
+		public long ParseTimeNs { get; set; }
+		public bool Valid { get; set; }
+		public int Size { get; set; }
+	}
+}
+
