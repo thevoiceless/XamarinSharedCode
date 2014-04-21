55f473964e599f32d50fdaf0f4b6af9de6f4d8e5
Riley Moses <victi.resurgo@gmail.com>
2014-04-21 12:15:13 -0600

    Move view controllers to their own dir, add past results screen

---
+++ iPhoneUsingCore/ViewControllers/PastResultsViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace iPhoneUsingCore
+{
+	public partial class PastResultsViewController : UIViewController
+	{
+		public PastResultsViewController () : base ("PastResultsViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+	}
+}
+
