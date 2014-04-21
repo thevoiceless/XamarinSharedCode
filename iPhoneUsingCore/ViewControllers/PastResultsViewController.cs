37a5e25c13ae8f72739ace7b3540c4e461f0f4b0
Riley Moses <victi.resurgo@gmail.com>
2014-04-21 15:35:31 -0600

    Remove obj dir

--- iPhoneUsingCore/ViewControllers/PastResultsViewController.cs
+++ iPhoneUsingCore/ViewControllers/PastResultsViewController.cs
@@ -1,22 +1,70 @@
 ﻿using System;
 using System.Drawing;
+using System.Collections.Generic;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using Core;
 
 namespace iPhoneUsingCore
 {
 	public partial class PastResultsViewController : UIViewController
 	{
-		public PastResultsViewController () : base ("PastResultsViewController", null)
+		public PastResultsViewController (IntPtr handle) : base (handle)
 		{
+			Console.WriteLine("********************** PastResultsViewController");
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+
+			DBManager db = DBManager.GetInstance();
+			this.pastResultsList.Source = new MyTableSource(db.GetAll<ValidatedJSON>());
 		}
+	}
+
+
+	/*
+	 * If you are looking at Objective-C code or documentation, be aware that Xamarin.iOS aggregates
+	 * the UITableViewDelegate protocol and UITableViewDataSource class into this one class. There is
+	 * no comparable UITableViewSource class or protocol in Objective-C.
+	 */
+	public class MyTableSource : UITableViewSource
+	{
+		private static NSString identifier = new NSString("cell");
+
+		private List<ValidatedJSON> entries;
+
+		public MyTableSource(List<ValidatedJSON> vals)
+		{
+			entries = vals;
+		}
+
+		public override int RowsInSection (UITableView tableview, int section)
+		{
+			return entries.Count;
+		}
+
+		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell(identifier);
+			if (cell == null)
+			{
+				cell = new UITableViewCell(UITableViewCellStyle.Default, identifier);
+			}
+			cell.TextLabel.Text = entries[indexPath.Row].AsJSON();
+			cell.TextLabel.Font = UIFont.FromName("Courier", 12);
+			cell.TextLabel.Lines = 100;
+			return cell;
+		}
+
+		public override float GetHeightForRow (UITableView tableView, NSIndexPath indexPath)
+		{
+			string json = entries[indexPath.Row].AsJSON();
+			int newlines = json.Split('\n').Length + 1;
+			return ((NSString) json).StringSize(UIFont.FromName("Courier", 12)).Height * newlines;
+		}
+
 	}
 }
 
