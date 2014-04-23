729d9ba4da1215309c96f7bc136cfd3ac19d7750
Riley Moses <victi.resurgo@gmail.com>
2014-04-23 14:18:39 -0600

    Use Xamarin.Mobile to get location in Android

--- AndroidUsingCore/Activities/MoreStuffActivity.cs
+++ AndroidUsingCore/Activities/MoreStuffActivity.cs
@@ -8,17 +8,26 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Geolocation;
 
 namespace AndroidUsingCore
 {
 	[Activity (Label = "@string/more_stuff")]			
 	public class MoreStuffActivity : Activity
 	{
+		Button locationButton;
+		EditText locationInfo;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.MoreStuff);
 			ActionBar.SetDisplayHomeAsUpEnabled(true);
+
+			locationButton = FindViewById<Button>(Resource.Id.locationButton);
+			locationInfo = FindViewById<EditText>(Resource.Id.locationInfo);
+
+			InitListeners();
 		}
 
 		public override bool OnOptionsItemSelected(IMenuItem item)
@@ -31,6 +40,18 @@ namespace AndroidUsingCore
 			}
 
 			return base.OnOptionsItemSelected(item);
+		}
+
+		void InitListeners()
+		{
+			locationButton.Click += async delegate {
+				var locator = new Geolocator(this) { DesiredAccuracy = 30 };
+				Position position = await locator.GetPositionAsync(10000);
+				string info = string.Format("Latitude: {0}\n Longitude: {1}\n Heading: {2}\n Speed: {3}\n Altitude: {4}\n Alt. Accuracy: {5}\n Overall Accuracy: {6}",
+					position.Latitude, position.Longitude, position.Heading, position.Speed, position.Altitude, position.AltitudeAccuracy, position.Accuracy);
+				locationInfo.Text = info;
+				Console.WriteLine(info);
+			};
 		}
 	}
 }
